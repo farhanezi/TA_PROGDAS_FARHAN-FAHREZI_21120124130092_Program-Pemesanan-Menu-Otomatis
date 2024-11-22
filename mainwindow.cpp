73774f7f2b88b98780e00aa6c2578b350c2fe333
farhanezi <esafitrioktorini@gmail.com>
2024-11-22 21:19:30 +0700

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -57,11 +57,11 @@ MainWindow::MainWindow(QWidget *parent)
 
     //effect transparansi
     QGraphicsOpacityEffect *effect13 = new QGraphicsOpacityEffect(this);
-    effect13->setOpacity(0); // Set opacity to 0 (invisible)
+    effect13->setOpacity(0); 
     ui->back_3->setGraphicsEffect(effect13);
 
     QGraphicsOpacityEffect *effect1 = new QGraphicsOpacityEffect(this);
-    effect1->setOpacity(0); // Set opacity to 0 (invisible)
+    effect1->setOpacity(0); 
     ui->bnmulai->setGraphicsEffect(effect1);
 
     QGraphicsOpacityEffect *effect2 = new QGraphicsOpacityEffect(this);
@@ -69,115 +69,115 @@ MainWindow::MainWindow(QWidget *parent)
     ui->bnsushi->setGraphicsEffect(effect2);
 
     QGraphicsOpacityEffect *effect3 = new QGraphicsOpacityEffect(this);
-    effect3->setOpacity(0); // Set opacity to 0 (invisible)
+    effect3->setOpacity(0); 
     ui->bnramen->setGraphicsEffect(effect3);
 
     QGraphicsOpacityEffect *effect4 = new QGraphicsOpacityEffect(this);
-    effect4->setOpacity(0); // Set opacity to 0 (invisible)
+    effect4->setOpacity(0); 
     ui->bnminum->setGraphicsEffect(effect4);
 
     QGraphicsOpacityEffect *effect5 = new QGraphicsOpacityEffect(this);
-    effect5->setOpacity(0); // Set opacity to 0 (invisible)
+    effect5->setOpacity(0); 
     ui->back->setGraphicsEffect(effect5);
 
     QGraphicsOpacityEffect *effect6 = new QGraphicsOpacityEffect(this);
-    effect6->setOpacity(0); // Set opacity to 0 (invisible)
+    effect6->setOpacity(0); 
     ui->back_2->setGraphicsEffect(effect6);
 
     QGraphicsOpacityEffect *effect7 = new QGraphicsOpacityEffect(this);
-    effect7->setOpacity(0); // Set opacity to 0 (invisible)
+    effect7->setOpacity(0); 
     ui->backfirst->setGraphicsEffect(effect7);
 
     QGraphicsOpacityEffect *effect8 = new QGraphicsOpacityEffect(this);
-    effect8->setOpacity(0); // Set opacity to 0 (invisible)
+    effect8->setOpacity(0); 
     ui->nextconfirm->setGraphicsEffect(effect8);
 
     QGraphicsOpacityEffect *effect9 = new QGraphicsOpacityEffect(this);
-    effect9->setOpacity(0); // Set opacity to 0 (invisible)
+    effect9->setOpacity(0); 
     ui->nextconfirm_2->setGraphicsEffect(effect9);
 
     QGraphicsOpacityEffect *effect10 = new QGraphicsOpacityEffect(this);
-    effect10->setOpacity(0); // Set opacity to 0 (invisible)
+    effect10->setOpacity(0); 
     ui->nextconfirm_3->setGraphicsEffect(effect10);
 
     QGraphicsOpacityEffect *effect11 = new QGraphicsOpacityEffect(this);
-    effect11->setOpacity(0); // Set opacity to 0 (invisible)
+    effect11->setOpacity(0); 
     ui->pushButton->setGraphicsEffect(effect11);
 
     QGraphicsOpacityEffect *effect12 = new QGraphicsOpacityEffect(this);
-    effect12->setOpacity(0); // Set opacity to 0 (invisible)
+    effect12->setOpacity(0); 
     ui->pushButton_2->setGraphicsEffect(effect12);
 
     QGraphicsOpacityEffect *effect14 = new QGraphicsOpacityEffect(this);
-    effect14->setOpacity(0); // Set opacity to 0 (invisible)
+    effect14->setOpacity(0); 
     ui->minramen1->setGraphicsEffect(effect14);
 
     QGraphicsOpacityEffect *effect15 = new QGraphicsOpacityEffect(this);
-    effect15->setOpacity(0); // Set opacity to 0 (invisible)
+    effect15->setOpacity(0); 
     ui->plusramen1->setGraphicsEffect(effect15);
 
     QGraphicsOpacityEffect *effect16 = new QGraphicsOpacityEffect(this);
-    effect16->setOpacity(0); // Set opacity to 0 (invisible)
+    effect16->setOpacity(0); 
     ui->minramen2->setGraphicsEffect(effect16);
 
     QGraphicsOpacityEffect *effect17 = new QGraphicsOpacityEffect(this);
-    effect17->setOpacity(0); // Set opacity to 0 (invisible)
+    effect17->setOpacity(0); 
     ui->plusramen2->setGraphicsEffect(effect17);
 
     QGraphicsOpacityEffect *effect18 = new QGraphicsOpacityEffect(this);
-    effect18->setOpacity(0); // Set opacity to 0 (invisible)
+    effect18->setOpacity(0); 
     ui->minramen3->setGraphicsEffect(effect18);
 
     QGraphicsOpacityEffect *effect19 = new QGraphicsOpacityEffect(this);
-    effect19->setOpacity(0); // Set opacity to 0 (invisible)
+    effect19->setOpacity(0); 
     ui->plusramen3->setGraphicsEffect(effect19);
 
     QGraphicsOpacityEffect *effect20 = new QGraphicsOpacityEffect(this);
-    effect20->setOpacity(0); // Set opacity to 0 (invisible)
+    effect20->setOpacity(0); 
     ui->minsushi1->setGraphicsEffect(effect20);
 
     QGraphicsOpacityEffect *effect21 = new QGraphicsOpacityEffect(this);
-    effect21->setOpacity(0); // Set opacity to 0 (invisible)
+    effect21->setOpacity(0); 
     ui->plussushi1->setGraphicsEffect(effect21);
 
     QGraphicsOpacityEffect *effect22 = new QGraphicsOpacityEffect(this);
-    effect22->setOpacity(0); // Set opacity to 0 (invisible)
+    effect22->setOpacity(0); 
     ui->minsushi2->setGraphicsEffect(effect22);
 
     QGraphicsOpacityEffect *effect23 = new QGraphicsOpacityEffect(this);
-    effect23->setOpacity(0); // Set opacity to 0 (invisible)
+    effect23->setOpacity(0); 
     ui->plussushi2->setGraphicsEffect(effect23);
 
     QGraphicsOpacityEffect *effect24 = new QGraphicsOpacityEffect(this);
-    effect24->setOpacity(0); // Set opacity to 0 (invisible)
+    effect24->setOpacity(0); 
     ui->minsushi3->setGraphicsEffect(effect24);
 
     QGraphicsOpacityEffect *effect25 = new QGraphicsOpacityEffect(this);
-    effect25->setOpacity(0); // Set opacity to 0 (invisible)
+    effect25->setOpacity(0); 
     ui->plussushi3->setGraphicsEffect(effect25);
 
     QGraphicsOpacityEffect *effect26 = new QGraphicsOpacityEffect(this);
-    effect26->setOpacity(0); // Set opacity to 0 (invisible)
+    effect26->setOpacity(0); 
     ui->minminum1->setGraphicsEffect(effect26);
 
     QGraphicsOpacityEffect *effect27 = new QGraphicsOpacityEffect(this);
-    effect27->setOpacity(0); // Set opacity to 0 (invisible)
+    effect27->setOpacity(0); 
     ui->plusminum1->setGraphicsEffect(effect27);
 
     QGraphicsOpacityEffect *effect28 = new QGraphicsOpacityEffect(this);
-    effect28->setOpacity(0); // Set opacity to 0 (invisible)
+    effect28->setOpacity(0); 
     ui->minminum2->setGraphicsEffect(effect28);
 
     QGraphicsOpacityEffect *effect29 = new QGraphicsOpacityEffect(this);
-    effect29->setOpacity(0); // Set opacity to 0 (invisible)
+    effect29->setOpacity(0); 
     ui->plusminum2->setGraphicsEffect(effect29);
 
     QGraphicsOpacityEffect *effect30 = new QGraphicsOpacityEffect(this);
-    effect30->setOpacity(0); // Set opacity to 0 (invisible)
+    effect30->setOpacity(0); 
     ui->minminum3->setGraphicsEffect(effect30);
 
     QGraphicsOpacityEffect *effect31 = new QGraphicsOpacityEffect(this);
-    effect31->setOpacity(0); // Set opacity to 0 (invisible)
+    effect31->setOpacity(0); 
     ui->plusminum3->setGraphicsEffect(effect31);
 }
 
@@ -198,7 +198,7 @@ void MainWindow::processMenu(QList<Menu> menuList) {
     }
 }
 
-void MainWindow::hal3tohal4(){ //sekkkkkkkkkkkkkkkk
+void MainWindow::hal3tohal4(){ 
     QList<Menu> menuList = {
         Menu("Minuman", "Matcha", valueminum1, 12000),
         Menu("Minuman", "Yuzu Lemon", valueminum2, 12000),
@@ -332,7 +332,7 @@ void MainWindow::hal3ramen(QString menu)
 void MainWindow::naikramen1()
 {
     valueramen1++; // Tambah nilai
-    ui->labelramen1->setText(QString::number(valueramen1)); // Update label
+    ui->labelramen1->setText(QString::number(valueramen1)); 
 }
 void MainWindow::turunramen1()
 {
@@ -340,14 +340,14 @@ void MainWindow::turunramen1()
         ui->labelramen1->setText(QString::number(0));
     }else{
     valueramen1--; // Kurangi nilai
-    ui->labelramen1->setText(QString::number(valueramen1)); // Update label
+    ui->labelramen1->setText(QString::number(valueramen1)); 
     }
 }
 
 void MainWindow::naikramen2()
 {
     valueramen2++; // Tambah nilai
-    ui->labelramen2->setText(QString::number(valueramen2)); // Update label
+    ui->labelramen2->setText(QString::number(valueramen2)); 
 }
 void MainWindow::turunramen2()
 {
@@ -355,14 +355,14 @@ void MainWindow::turunramen2()
         ui->labelramen2->setText(QString::number(0));
     }else{
     valueramen2--; // Kurangi nilai
-    ui->labelramen2->setText(QString::number(valueramen2)); // Update label
+    ui->labelramen2->setText(QString::number(valueramen2)); 
     }
 }
 
 void MainWindow::naikramen3()
 {
     valueramen3++; // Tambah nilai
-    ui->labelramen3->setText(QString::number(valueramen3)); // Update label
+    ui->labelramen3->setText(QString::number(valueramen3)); 
 }
 void MainWindow::turunramen3()
 {
@@ -370,7 +370,7 @@ void MainWindow::turunramen3()
         ui->labelramen3->setText(QString::number(0));
     }else{
     valueramen3--; // Kurangi nilai
-    ui->labelramen3->setText(QString::number(valueramen3)); // Update label
+    ui->labelramen3->setText(QString::number(valueramen3)); 
     }
 }
 
@@ -395,7 +395,7 @@ void MainWindow::hal3sushi(QString menu)
 void MainWindow::naiksushi1()
 {
     valuesushi1++; // Tambah nilai
-    ui->labelsushi1->setText(QString::number(valuesushi1)); // Update label
+    ui->labelsushi1->setText(QString::number(valuesushi1)); 
 }
 void MainWindow::turunsushi1()
 {
@@ -403,14 +403,14 @@ void MainWindow::turunsushi1()
         ui->labelsushi1->setText(QString::number(0));
     }else{
     valuesushi1--; // Kurangi nilai
-    ui->labelsushi1->setText(QString::number(valuesushi1)); // Update label
+    ui->labelsushi1->setText(QString::number(valuesushi1)); 
     }
 }
 
 void MainWindow::naiksushi2()
 {
     valuesushi2++; // Tambah nilai
-    ui->labelsushi2->setText(QString::number(valuesushi2)); // Update label
+    ui->labelsushi2->setText(QString::number(valuesushi2)); 
 }
 void MainWindow::turunsushi2()
 {
@@ -418,14 +418,14 @@ void MainWindow::turunsushi2()
         ui->labelsushi2->setText(QString::number(0));
     }else{
     valuesushi2--; // Kurangi nilai
-    ui->labelsushi2->setText(QString::number(valuesushi2)); // Update label
+    ui->labelsushi2->setText(QString::number(valuesushi2)); 
     }
 }
 
 void MainWindow::naiksushi3()
 {
     valuesushi3++; // Tambah nilai
-    ui->labelsushi3->setText(QString::number(valuesushi3)); // Update label
+    ui->labelsushi3->setText(QString::number(valuesushi3)); 
 }
 void MainWindow::turunsushi3()
 {
@@ -433,7 +433,7 @@ void MainWindow::turunsushi3()
         ui->labelsushi3->setText(QString::number(0));
     }else{
     valuesushi3--; // Kurangi nilai
-    ui->labelsushi3->setText(QString::number(valuesushi3)); // Update label
+    ui->labelsushi3->setText(QString::number(valuesushi3)); 
     }
 }
 
